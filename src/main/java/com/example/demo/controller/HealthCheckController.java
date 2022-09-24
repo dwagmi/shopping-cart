a959200d5a8abba2934afb31c44ea02d03ce47f0
Nissan Silvia <200sxmeda@gmail.com>
2022-09-24 18:18:58 +1000

    First commit

---
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping(method = RequestMethod.GET, value="/health")
+    public HttpStatus checkHealth() {
+        return HttpStatus.OK;
+    }
+}
