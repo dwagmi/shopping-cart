15383ea1c3a53604be56e8f169ece2d9b294ea42
Nissan Silvia <200sxmeda@gmail.com>
2022-09-25 11:10:50 +1000

    Improve error handling

--- src/main/java/com/example/demo/api/CartApi.java
+++ src/main/java/com/example/demo/api/CartApi.java
@@ -8,14 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/cart")
+@RequestMapping("/cart")
 public class CartApi {
     Logger log = LoggerFactory.getLogger(CartApi.class);
 
@@ -26,12 +26,17 @@ public class CartApi {
         this.cartService = cartService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value="/list")
+    @GetMapping(path="/all")
     public ResponseEntity<List<Cart>> getAllCarts() {
         List<Cart> allCarts = cartService.getAllCarts();
         log.info("Retrieved all carts: " + allCarts);
         return ResponseEntity.status(HttpStatus.OK).body(allCarts);
     }
 
-
+    @GetMapping(path="/{cartId}")
+    public ResponseEntity<Cart> getCartById(@PathVariable Long cartId) {
+        Cart cart = cartService.getCartById(cartId);
+        log.info("Retrieved cart: " + cart);
+        return ResponseEntity.status(HttpStatus.OK).body(cart);
+    }
 }
--- src/main/java/com/example/demo/error/ErrorApi.java
+++ src/main/java/com/example/demo/error/ErrorApi.java
@@ -1,20 +1,49 @@
 package com.example.demo.error;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 /**
  * Default error handler will return 400 by default.
  */
 @RestController
-public class ErrorApi implements ErrorController {
+@ControllerAdvice
+public class ErrorApi extends ResponseEntityExceptionHandler implements ErrorController {
 
+    /**
+     * Default error handling, used as a fall-back if no specific error handler
+     * exists for a particular Exception.
+     *
+     * @return
+     */
     @RequestMapping(value = "/error")
     public ResponseEntity<String> error() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request");
+    }
+
+
+    /**
+     * Handles type mismatches in path variables of API request.
+     *
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(value = { MethodArgumentTypeMismatchException.class })
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException e, WebRequest request) {
+        String bodyOfResponse = "Input format is incorrect";
+        return handleExceptionInternal(e, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 }
